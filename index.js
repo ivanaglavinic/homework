090ec4b3e0c2823f589fbffe1f555e2aca18fded
ivanaglavinic <ivanaglavinic8@gmail.com>
2025-02-20 23:44:43 +0100

    finished java script week 2

--- index.js
+++ index.js
@@ -134,11 +134,131 @@ function scopeTest() {
 
 scopeTest();
 
-
 //for loop
-function listNumbers(amount){
-for (let i=0; i < amount; i++) {
-let numbers = "";
-}}
+function listNumbers(amount) {
+  for (let i = 0; i < amount; i++) {
+    let numbers = "";
+  }
+}
 
 listNumbers(24);
+
+//WEEK 2 HOMEWORK
+
+function getFullName(
+  firstName,
+  surname,
+  useFormalName = false,
+  gender = "male"
+) {
+  let fullname = firstName + " " + surname;
+
+  //name not inserted
+  if (!firstName || !surname) {
+    return "Both first and last names are required!";
+  }
+  // adding formal name and gender
+  if (useFormalName) {
+    if (gender === "female") {
+      return "Lady " + fullname;
+    } else {
+      return "Lord " + fullname;
+    }
+  } else {
+    return fullname;
+  }
+}
+
+getFullName("Benjamin", "Hughes");
+
+let fullname1 = getFullName("Sarah", "Johnson", true, "female");
+let fullname2 = getFullName("Mark", "Cuban", true, "male");
+
+console.log(fullname1);
+console.log(fullname2);
+
+//
+function getEventWeekday(daysFromToday) {
+  const today = new Date();
+  const todayDay = today.getDay();
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const eventDay = (todayDay + daysFromToday) % 7;
+
+  return weekdays[eventDay];
+}
+console.log(getEventWeekday(4));
+
+//
+
+function whatShouldIWear(temperature) {
+  if (temperature < 0) {
+    return "coat, gloves, and a scarf";
+  } else if (temperature >= 0 && temperature < 10) {
+    return "jacket and warm pants";
+  } else if (temperature >= 10 && temperature < 20) {
+    return "sweater or a long-sleeve shirt";
+  } else {
+    return "shorts and a t-shirt";
+  }
+}
+const clothesToWear = whatShouldIWear(15);
+console.log("You should wear " + clothesToWear);
+
+//
+
+const class07Students = [];
+
+function addStudentToClass(studentName) {
+  // Check if studentName is empty
+  if (studentName === "") {
+    console.log("Cannot add an empty string to the class");
+    return;
+  }
+
+  // if "Queen" is being added
+  if (studentName === "Queen") {
+    if (!class07Students.includes("Queen")) {
+      class07Students.push(studentName);
+    }
+    return;
+  }
+
+  // if student is already in the class
+  if (class07Students.includes(studentName)) {
+    console.log(`Student ${studentName} is already in the class`);
+    return;
+  }
+
+  // if class already has 6 students
+  if (class07Students.length >= 6) {
+    console.log("Cannot add more students to class 07");
+    return;
+  }
+
+  // Add the student
+  class07Students.push(studentName);
+}
+// check how many students
+function getNumberOfStudents() {
+  return class07Students.length;
+}
+
+addStudentToClass("Maria");
+addStudentToClass("Ana");
+addStudentToClass("Marko");
+addStudentToClass("David");
+addStudentToClass("Sara");
+addStudentToClass("Frank");
+addStudentToClass("Deborah");
+addStudentToClass("Ana");
+addStudentToClass("Queen");
+
+console.log(getNumberOfStudents());
